e9bea3110c45b92cb0ba3fb3dec9c4037e3a542b
Paul Pinard <paul.k.pinard@gmail.com>
2022-09-26 13:22:12 -0400

    create department table

--- schema.sql
+++ schema.sql
@@ -2,3 +2,8 @@ DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
 
 USE employees_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
+);
