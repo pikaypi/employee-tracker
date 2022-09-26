0d82a36330e81a996df27687c2b37a5f8e8117ff
Paul Pinard <paul.k.pinard@gmail.com>
2022-09-26 13:24:11 -0400

    create role table

--- schema.sql
+++ schema.sql
@@ -7,3 +7,10 @@ CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30)
 );
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title NOT NULL VARCHAR(30),
+    salary NOT NULL DECIMAL,
+    department_id INT NOT NULL
+);
