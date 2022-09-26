6a6548a84ac459bcfbbcb8a0497bc76c37bdf4da
Paul Pinard <paul.k.pinard@gmail.com>
2022-09-26 13:28:02 -0400

    create employee table

--- schema.sql
+++ schema.sql
@@ -10,10 +10,18 @@ CREATE TABLE department (
 
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title NOT NULL VARCHAR(30),
-    salary NOT NULL DECIMAL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
     ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL
 );
